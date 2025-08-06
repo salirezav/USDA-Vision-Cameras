593ab7ae88eb22910b670424885ae1bb6f394b97
Alireza Vaezi <s.alireza.v@gmail.com>
2025-08-06 11:54:27 -0400

    Revert "Master"

--- tests/legacy_tests/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from usda-vision-cameras!")
-
-
-if __name__ == "__main__":
-    main()
--- usda_vision_system/__main__.py
+++
@@ -1,8 +0,0 @@
-"""
-Entry point for running the USDA Vision Camera System as a module.
-"""
-
-from .main import main
-
-if __name__ == "__main__":
-    main()
